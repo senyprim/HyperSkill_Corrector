7317eef0f64bf440c3819465b0776d84273fcc8f
senyprim <senyprim105@gmail.com>
2020-06-25 11:01:07 +1000

    Stage 5

--- Error Correcting Encoder-Decoder/task/src/correcter/Hamming.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Hamming.java
@@ -4,11 +4,8 @@ import java.util.Random;
 
 public class Hamming {
     private static byte encode(byte data){
-        byte mask= (byte) ((data&128)>>2|(data&64)>>3|(data&32)>>3|(data&16)>>3);
-        mask=(byte) (mask
-                |((mask&32)>>5^(mask&8)>>3^(mask&2)>>1)<<7
-                |((mask&32)>>5^(mask&4)>>2^(mask&2)>>1)<<6
-                |((mask&16)>>4^(mask&8)>>3^(mask&4)>>2^(mask&2)>>1)<<4);
+        byte mask= (byte) ((data&128)>>>2|(data&64)>>3|(data&32)>>3|(data&16)>>3);
+        mask=(byte) (mask | calculateCheckBits(mask));
         return mask;
    }
    public static byte[] encode(byte[] data){
@@ -31,20 +28,19 @@ public class Hamming {
        return (byte) (data&128|data&64|data&16);
    }
    private static byte calculateCheckBits(byte mask){
-        byte result=(byte)
+        return (byte)
                 (((mask&32)>>5^(mask&8)>>3^(mask&2)>>1)<<7
                 |((mask&32)>>5^(mask&4)>>2^(mask&2)>>1)<<6
-                |((mask&16)>>4^(mask&8)>>3^(mask&4)>>2^(mask&2)>>1)<<4);
-        return result;
+                |((mask&8)>>3^(mask&4)>>2^(mask&2)>>1)<<4);
    }
-    public static byte fix(byte data){
-        byte change=(byte)(getCheckBits(data)^calculateCheckBits(data));
+
+   private static byte fix(byte data){
+        byte getcheck=getCheckBits(data);
+        byte getcalck=calculateCheckBits(data);
+        byte change=(byte)(getcheck^getcalck);
         if (change==0) return data;
-        int errorPosition=0;
-        for(int position=0;position<4;position++){
-            errorPosition+=(change<<position&128)*position;
-        }
-        return (byte)(128>>errorPosition^data);
+        int errorPosition=(change>>7&1)+(change>>6&1)*2+(change>>4&1)*4;
+        return (byte)(128>>(errorPosition-1)^data);
    }
    public static byte[] fix(byte[] data){
         byte[] result = new byte[data.length];
@@ -53,14 +49,14 @@ public class Hamming {
         }
         return result;
    }
-   private static byte decode(byte data){
-       return (byte) ((data&32)<<2|(data&8)<<3|(data&4)<<3|(data&2)<<3);
-   }
 
+   private static byte decode(byte data){
+        return (byte) ((data&32)<<2|(data&8)<<3|(data&4)<<3|(data&2)<<3);
+   }
    public static byte[] decode(byte[] data){
         byte[] result= new byte[data.length/2];
         for(int index=0;index<result.length;index++){
-            result[index]=(byte) (decode(data[index*2])|decode(data[index*2+1])>>4);
+            result[index]=(byte) (decode(data[index*2])|(decode(data[index*2+1])&0xFF)>>4);
         }
         return result;
    }
--- Error Correcting Encoder-Decoder/task/src/correcter/Main.java
+++ Error Correcting Encoder-Decoder/task/src/correcter/Main.java
@@ -8,70 +8,30 @@ import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.Random;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
-        Hamming.fix((byte) Integer.parseInt("01000010",2));
         encode();
         send();
         decode();
     }
     public static void encode() throws IOException {
         byte[] file = Files.readAllBytes(Paths.get("send.txt"));
-        for(byte b : file){
-            System.out.print(Integer.toBinaryString((b & 0xFF) + 0x100).substring(1));
-            System.out.print(" ");
-        }
-        System.out.println();
-        byte[] encode = Hamming.encode(file);
-        for(byte b : encode){
-            System.out.print(Integer.toBinaryString((b & 0xFF) + 0x100).substring(1));
-            System.out.print(" ");
-        }
-        System.out.println();
         try(FileOutputStream out = new FileOutputStream("encoded.txt" )){
-            out.write(encode);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            out.write(Hamming.encode(file));
         }
     }
     public static void send() throws IOException {
         byte[] file = Files.readAllBytes(Paths.get("encoded.txt"));
-        byte[] send = Hamming.send(file,new Random());
-        for(byte b : send){
-            System.out.print(Integer.toBinaryString((b & 0xFF) + 0x100).substring(1));
-            System.out.print(" ");
-        }
-        System.out.println();
         try(FileOutputStream out = new FileOutputStream("received.txt" )){
-            out.write(send);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            out.write(Hamming.send(file,new Random()));
         }
     }
     public static void decode() throws IOException {
         byte[] file = Files.readAllBytes(Paths.get("received.txt"));
-        byte[] fix = Hamming.fix(file);
-        for(byte b : fix){
-            System.out.print(Integer.toBinaryString((b & 0xFF) + 0x100).substring(1));
-            System.out.print(" ");
-        }
-        System.out.println();
-        byte[] decode = Hamming.decode(file);
-        for(byte b : decode){
-            System.out.print(Integer.toBinaryString((b & 0xFF) + 0x100).substring(1));
-            System.out.print(" ");
-        }
-        System.out.println();
+        byte[] decode = Hamming.decode(Hamming.fix(file));
         try(FileOutputStream out = new FileOutputStream("decoded.txt" )){
             out.write(decode);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
         }
     }
 
